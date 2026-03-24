6f136ba6d5ee55c173e6641ca22c943be6e17be3
code-arif <arifulislam6460@gmail.com>
2026-03-24 14:45:32 +0600

    update vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 export default defineConfig({
-  plugins: [vue(),
-    tailwindcss()
-  ],
-})
+  plugins: [vue()],
+  resolve: { alias: { "@": resolve(__dirname, "src") } },
+  server: {
+    port: 3001,
+    proxy: { "/api": { target: "http://localhost:8000", changeOrigin: true } },
+  },
+});
