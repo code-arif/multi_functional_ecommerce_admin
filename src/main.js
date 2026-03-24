604c0a43be968e658cd42d200ea12c48d3e74544
code-arif <arifulislam6460@gmail.com>
2026-03-24 14:05:00 +0600

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import Toast, { POSITION } from 'vue-toastification'
 import App from './App.vue'
+import router from './router'
+import 'vue-toastification/dist/index.css'
+import './assets/css/main.css'
 
-createApp(App).mount('#app')
+const app   = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia).use(router)
+app.use(Toast, { position: POSITION.TOP_RIGHT, timeout: 3000, maxToasts: 3 })
+
+import { useAuthStore } from './stores/auth'
+useAuthStore(pinia).fetchMe().then(() => app.mount('#app'))
