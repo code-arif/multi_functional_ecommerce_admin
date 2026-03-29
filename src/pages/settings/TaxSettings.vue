5b409c725c5d6ac618fd4c7c04afb5c69b0f0db0
code-arif <arifulislam6460@gmail.com>
2026-03-29 14:35:38 +0600

    feat: add TaxSettings page for configuring store tax rates and display options

---
+++ src/pages/settings/TaxSettings.vue
@@ -0,0 +1,67 @@
+<template>
+    <SettingsCard :icon="Percent" title="Tax & VAT" description="Configure tax rates for your store" :saving="saving"
+        @save="save">
+        <div class="field-group">
+            <div class="field col-span-2">
+                <div class="toggle-row">
+                    <div class="toggle-info">
+                        <span class="toggle-info__title">Enable Tax</span>
+                        <span class="toggle-info__desc">Apply tax to orders</span>
+                    </div>
+                    <label class="toggle">
+                        <input type="checkbox" v-model="form.tax_enabled" true-value="1" false-value="0" />
+                        <span class="toggle__slider"></span>
+                    </label>
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Tax Rate (%)</label>
+                <div class="input-group">
+                    <input v-model="form.tax_rate" type="number" class="input" placeholder="15" step="0.01" />
+                    <span class="input-group__prefix" style="right:11px;left:auto;font-size:0.8rem">%</span>
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Tax Name / Label</label>
+                <input v-model="form.tax_name" class="input" placeholder="VAT" />
+            </div>
+            <div class="field">
+                <label class="label">Tax Calculation</label>
+                <select v-model="form.tax_type" class="select">
+                    <option value="inclusive">Inclusive (included in price)</option>
+                    <option value="exclusive">Exclusive (added on top)</option>
+                </select>
+            </div>
+            <div class="field">
+                <label class="label">Tax Based On</label>
+                <select v-model="form.tax_based_on" class="select">
+                    <option value="billing_address">Billing Address</option>
+                    <option value="shipping_address">Shipping Address</option>
+                    <option value="store_address">Store Address</option>
+                </select>
+            </div>
+            <div class="field col-span-2">
+                <div class="toggle-row">
+                    <div class="toggle-info">
+                        <span class="toggle-info__title">Display Tax in Cart</span>
+                        <span class="toggle-info__desc">Show tax breakdown to customers</span>
+                    </div>
+                    <label class="toggle">
+                        <input type="checkbox" v-model="form.tax_display_in_cart" true-value="1" false-value="0" />
+                        <span class="toggle__slider"></span>
+                    </label>
+                </div>
+            </div>
+        </div>
+    </SettingsCard>
+</template>
+
+<script setup>
+import { Percent } from 'lucide-vue-next'
+import SettingsCard from '../components/SettingsCard.vue'
+import { useSettings } from '../useSettings'
+
+const keys = ['tax_enabled', 'tax_rate', 'tax_name', 'tax_type', 'tax_based_on', 'tax_display_in_cart']
+const { form, saving, save } = useSettings(keys, 'Tax')
+</script>
